5c0b9497083334d077999e7ee1995962769fe5d3
nguyenduong1803 <duonghd1803@gmail.com>
2022-11-08 08:38:54 +0700

    add

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Routers from "./routers";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+      <Routers />
+  </React.StrictMode>
+);
